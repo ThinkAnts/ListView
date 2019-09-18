075c0ee7552dab96666030fedaa34d0c06b13a92
Ravi Kishore.K <ravi.kankulakunta@sourcebits.com>
2019-09-18 21:53:32 +0530

    Added Pods for reachability and implemented in Tableview

--- ListView/ViewController.swift
+++ ListView/ViewController.swift
@@ -7,14 +7,50 @@
 //
 
 import UIKit
+import Reachability
+
+enum ActivityState {
+    case inactive
+    case loadingLocal
+    case loadingRemote
+}
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    var reachability: Reachability!
+
+    @IBOutlet weak var tableViewTopSpaceConstraint: NSLayoutConstraint!
+    @IBOutlet weak var activityLabel: UILabel!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var tableView: UITableView!
+
+    var state: ActivityState = .inactive {
+        didSet {
+            switch state {
+            case .inactive:
+                self.activityIndicator.stopAnimating()
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+                    self.activityLabel.text = ""
+                    UIView.animate(withDuration: 0.50) {
+                        self.tableViewTopSpaceConstraint.constant = 0.0
+                        self.view.layoutIfNeeded()
+                    }
+                }
+            default:
+                self.activityIndicator.startAnimating()
+                self.activityLabel.text = state == .loadingLocal ? "Loading data from cache" : "Downloading data from server"
+                UIView.animate(withDuration: 0.50) {
+                    self.tableViewTopSpaceConstraint.constant = 60.0
+                    self.view.layoutIfNeeded()
+                }
+            }
+        }
     }
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.reachability = Reachability()
+        self.state = .loadingLocal
+    }
 
 }
-
---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'ListView' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for ListView
+  pod 'ReachabilitySwift'
+
+  target 'ListViewTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'ListViewUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
