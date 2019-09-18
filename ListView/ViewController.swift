6b9c7b5aa85cbaf03515b22dd30634ff210c0ac8
Ravi Kishore.K <ravi.kankulakunta@sourcebits.com>
2019-09-18 20:18:44 +0530

    Initial Commit

---
+++ ListView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ListView
+//
+//  Created by Ravi on 18/09/19.
+//  Copyright © 2019 ThinkAnts. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
