2de8ad9e784093669f01c6a0e6af3284f90a2595
Ravi Kishore <ravi.kankulakunta@sourcebits.com>
2019-09-22 22:27:42 +0530

    youtube model added.

---
+++ ListView/View Model/YoutubeModel.swift
@@ -0,0 +1,12 @@
+//
+//  YoutubeModel.swift
+//  ListView
+//
+//  Created by Ravi Kishore on 9/22/19.
+//  Copyright © 2019 ThinkAnts. All rights reserved.
+//
+
+import Foundation
+
+final class YoutubeModel {
+}
